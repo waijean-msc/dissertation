0ee7b090d7fb8ef201a898e81e100d901ba2ff79
Wai Jean Koh <kwj_97@hotmail.com>
2023-04-23 16:41:17 +0100

    analysisv1

--- model/finbert_pred.py
+++ model/finbert_pred.py
@@ -14,7 +14,7 @@ def query(url, sentences):
 
     response = requests.post(url, headers=headers, json=payload)
 
-    return response.json()
+    return response
 
 
 def split(list_a, chunk_size):
@@ -23,15 +23,14 @@ def split(list_a, chunk_size):
 
 
 if __name__ == '__main__':
-    # API_URL = "https://api-inference.huggingface.co/models/ProsusAI/finbert"
-    API_URL = "https://api-inference.huggingface.co/models/yiyanghkust/finbert-tone"
-    model_name = "finbert_v2"
+    API_URL = "https://api-inference.huggingface.co/models/ProsusAI/finbert"
+    # API_URL = "https://api-inference.huggingface.co/models/yiyanghkust/finbert-tone"
+    model_name = "finbert_v1"
 
-    df = pd.read_csv("../news_stock_price.csv")
-    df["summary_title"] = df["summary"].combine_first(df["title"])
+    df = pd.read_csv("../news/news_v2/analysis/news_v2.1.csv")
 
     # convert to list
-    summary_title_list = df["summary_title"].to_list()
+    summary_title_list = df["model_input"].to_list()
     # check all string instances
     all([isinstance(sent, str) for sent in summary_title_list])
 
@@ -44,25 +43,42 @@ if __name__ == '__main__':
     p_list = []
     for i, l in enumerate(chunk_summary_title_list):
         # skip the first n if it has been processed
-        if i < 1418:
+        if i < 379:
             continue
         print(f"Processing {i}/{len(chunk_summary_title_list)}")
         print(f"Number of sentences per chunk list: {len(l)}")
         # truncate maximum number of characters for the model (512)
         max_char = 512
         truncated_l = [sent[:max_char] if len(sent) > max_char else sent for sent in l]
-        try:
-            p = query(url=API_URL, sentences=truncated_l)
-        except:
-            print("Error with API")
 
-        if len(p) == len(l):
-            p_list.extend(p)
-        else:
-            print(f"API returns unexpected length: {len(p)}: {p}")
+        # retry for 10 times in case server is unavailable
+        for retry in range(0, 10):
+            try:
+                response = query(url=API_URL, sentences=truncated_l)
+                p = response.json()
+            except Exception as e:
+                print(e)
+                if retry == 9:
+                    raise
+                else:
+                    continue
+            else:
+                if len(p) == len(l):
+                    break
+                elif 'error' in p:
+                    if 'Rate limit reached.' in p["error"]:
+                        raise Exception
+                else:
+                    print(f"API returns unexpected length: {len(p)}: {p}")
+                    if retry == 9:
+                        raise Exception
+                    else:
+                        continue
+
+        p_list.extend(p)
 
     # df = pd.DataFrame(p_list)
-    # df.to_csv(f"{len(p_list)}_{model_name}_part2.csv", index=False)
+    # df.to_csv(f"{model_name}_part3_{len(p_list)}.csv", index=False)
 
     df[f"{model_name}_result"] = p_list
     df.to_csv(f"{model_name}_pred.csv", index=False)
